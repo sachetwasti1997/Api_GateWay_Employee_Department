13919676bc2ab86544d088c625abf3a300c58248
sachetwasti1997 <sachetwasti61@gmail.com>
2023-04-17 10:49:35 +0530

    Project Initialisation

---
+++ src/main/java/com/sachet/api_gateway_department_employee/ApiGatewayDepartmentEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.sachet.api_gateway_department_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayDepartmentEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayDepartmentEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachet/api_gateway_department_employee/ApiGatewayDepartmentEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.api_gateway_department_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayDepartmentEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
